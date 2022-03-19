1316cc635fc4b2f5f5e4e30a4a30bf33892f239f
Madiyar <example@gmail.com>
2022-03-20 00:32:52 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+"use sctrict";
+//let number =4;
+//console.log(4/0);
+//console.log('string'*9);
+
+//const persone = "5";
+
+//const bool = true;
+
+//console.log(something);
+
+//let und;
+//console.log(und);
+//-----------------------------------
+//const obj = {
+  //  name: "John",
+    //age:25,
+    //isMarried:false
+
+
+//};
+//console.log(obj.name);
+// ОбЪекты ключевая еденица JavaScripta
+//Является ли Массив одтельным типом данным ? нет массив частный случай 
+//let arr = ['plum.png', 'orange.jpg', 6 , 'apple.bmp', {}, []]
+//console.log(arr[1]);
+//-----------------------------------
+
+//alert('Hello ');
+//const result =confirm("Are you here ?");
+
+//const answer =+prompt("Вам есть 18? ", "18");
+//console.log(answer+5);
+//const answers =[];
+//answers[0] =prompt("Your name?", '');
+//answers[1] =prompt("Your surname?", '');
+//answers[2] =prompt("Your age?", '');
+
+//document.write(answers);
+// Взоимодействия сайта
+//-------------------------------------------------------------------------------
+
+// ---------------------------------------------------------
+// Интерполяция
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+const user ='Ivan';
+console.log(`Do you want to eat ${user} ?`);
+
